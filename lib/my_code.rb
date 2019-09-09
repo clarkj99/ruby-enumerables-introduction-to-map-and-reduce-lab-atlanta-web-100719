79ea7acddb8e59754fea352f88fe1279f61dbb2f
considerate-glossary-7180 <clarkandkathy@gmail.com>
2019-09-09 20:45:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,7 +1,7 @@
 # My Code here....
 def map-to-negativize (source_array) {
   new_array =[]
-  while source_array.length.times  {|index|
+  source_array.length.times  {|index|
     new_array.push(source_array[index] * -1)
   }
   new_array
